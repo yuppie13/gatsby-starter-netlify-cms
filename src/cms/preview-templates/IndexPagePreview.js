9dbfb09a0726c3fd4ee9ed6a18ad2c3bf8a2b172
yuppie13 <52652381+yuppie13@users.noreply.github.com>
2021-03-14 20:31:21 +0000

    Update IndexPagePreview.js

--- src/cms/preview-templates/IndexPagePreview.js
+++ src/cms/preview-templates/IndexPagePreview.js
@@ -12,9 +12,7 @@ const IndexPagePreview = ({ entry, getAsset }) => {
         title={data.title}
         heading={data.heading}
         subheading={data.subheading}
-        description={data.description}
         intro={data.intro || { blurbs: [] }}
-        mainpitch={data.mainpitch || {}}
       />
     )
   } else {
